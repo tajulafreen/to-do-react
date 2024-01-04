fec00fc744dea17efeafe8f123e5295ba49c442e
afreen shaik <affushaik9493@gmail.com>
2024-01-05 03:44:10 +0530

    add styles

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -8,8 +8,12 @@ const AddTask = ({ addTask, newTask, handleInputChange }) => {
         placeholder="Enter to do..."
         value={newTask}
         onChange={handleInputChange}
+        className="w-[70%] p-3 rounded-lg text-[16px]"
       />
-      <button className="add-btn" onClick={addTask}>
+      <button
+        className="bg-green-500 mx-5 w-[80px] rounded-lg p-2 text-gray-200 text-[20px] hover:bg-green-600"
+        onClick={addTask}
+      >
         Add
       </button>
     </div>
