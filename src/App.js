cc66cfd68fb032e6a920bca8ebb9c36af0148f7e
afreen shaik <affushaik9493@gmail.com>
2024-01-05 03:43:57 +0530

    add height and  width for app

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import ToDoList from "./components/ToDoList";
 function App() {
   return (
-    <div className="App">
+    <div className="App justify-center h-[80%] lg:h-[60%] w-[90%] lg:w-[60%] m-auto my-[10%]">
       <ToDoList />
     </div>
   );
